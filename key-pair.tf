81968e45b54e33dc0bdfbed8ca46d951ffc44c09
newabdosheham <abd2007sh@hotmail.com>
2024-12-30 16:09:03 +0200

    Update key-pair.tf

--- key-pair.tf
+++ key-pair.tf
@@ -1,4 +1,4 @@
-resource "aws_key_pair" "example" {
+resource "aws_key_pair" "example2" {
   key_name   = var.key_name 
   public_key = file("${path.module}/mykey.pub")  # Replace the path for your public key file
 }
