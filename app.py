42d904899cf7359b3cfb1c8949de4e409214b819
newabdosheham <abd2007sh@hotmail.com>
2024-12-30 14:10:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html')  # Flask automatically looks for templates in the 'templates' folder
 
 if __name__ == '__main__':
-    app.run(debug=True, port=80) # App will be Expose in Port 80
+    app.run(debug=True, port=80)  # The app will be exposed on port 80
+
