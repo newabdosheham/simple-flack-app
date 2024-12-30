2be8ad30c21e3a96c30a289d96462ecee1b2b0ea
newabdosheham <abd2007sh@hotmail.com>
2024-12-30 13:48:18 +0200

    Update key-pair.tf

--- key-pair.tf
+++ key-pair.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "example" {
   key_name   = var.key_name 
-  public_key = file("/mnt/c/terraform/projects/simple-flask-aap/mykey.pub")  # Replace the path for your public key file
+  public_key = file("mykey.pub")  # Replace the path for your public key file
 }
