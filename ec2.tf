83d33e3615c358931506614b83c25ff6d19b1bf6
newabdosheham <abd2007sh@hotmail.com>
2024-12-30 13:51:06 +0200

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -8,13 +8,13 @@ resource "aws_instance" "server" {
   connection {
     type        = "ssh"
     user        = "ubuntu"  # Replace with the appropriate username for your EC2 instance
-    private_key = file("mykey")  # Replace with the path to your private key
+    private_key = file("${path.module}/mykey")  # Replace with the path to your private key
     host        = self.public_ip
    }
 
    # File provisioner to copy a file from local to the remote EC2 instance
    provisioner "file" {
-    source      = "app.py"  # Replace with the path to your local file
+    source      = file("${path.module}/app.py")  # Replace with the path to your local file
     destination = "/home/ubuntu/app.py"  # Replace with the path on the remote instance
    }
 
